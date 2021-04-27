b596a695b05534cb6cc68b6dbae7fd2cdd0a2fad
S4B1K24 <83269819+S4B1K24@users.noreply.github.com>
2021-04-27 15:39:49 +0700

    Update some_app.py

--- some_app.py
+++ some_app.py
@@ -1,3 +1,12 @@
+print("Hello world")
+from flask import Flask
+app = Flask(__name__)
+#декоратор для вывода страницы по умолчанию
+@app.route("/")
+def hello():
+ return " <html><head></head> <body> Hello World! </body></html>"
+if __name__ == "__main__":
+ app.run(host='127.0.0.1',port=5000) 
 from flask import render_template
 from flask import Flask
 #наша новая функция сайта
