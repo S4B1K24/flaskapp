195d1065f39e5dbc2f3e278cacd7bf1603852cae
S4B1K24 <andrej.alin.02@mail.ru>
2021-04-27 15:42:40 +0700

    Update some_app.py

--- some_app.py
+++ some_app.py
@@ -8,9 +8,6 @@ def hello():
 if __name__ == "__main__":
  app.run(host='127.0.0.1',port=5000) 
 from flask import render_template
-from flask import Flask
-#наша новая функция сайта
-app = Flask(__name__)
 @app.route("/data_to")
 def data_to():
  #создаем переменные с данными для передачи в шаблон
@@ -18,5 +15,4 @@ def data_to():
  some_str = 'Hello my dear friends!'
  some_value = 10
  #передаем данные в шаблон и вызываем его
- return render_template('simple.html',some_str = some_str,
- some_value = some_value,some_pars=some_pars) 
+ return render_template('simple.html',some_str = some_str, some_value = some_value,some_pars=some_pars)
