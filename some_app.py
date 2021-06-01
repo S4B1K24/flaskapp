50b2360a79776783810cbd9f04c42b14ab36b9df
S4B1K24 <andrej.alin.02@mail.ru>
2021-06-01 21:31:12 +0700

    Update some_app.py

--- some_app.py
+++ some_app.py
@@ -1,4 +1,5 @@
 from flask import render_template
+<<<<<<< Updated upstream
 from flask import Flask
 app = Flask(__name__)
 @app.route("/data_to")
@@ -47,3 +48,15 @@ status=200,
 mimetype="application/json")
 return resp
 
+=======
+#наша новая функция сайта
+@app.route("/data_to")
+def data_to():
+ #создаем переменные с данными для передачи в шаблон
+ some_pars = {'user':'Ivan','color':'red'}
+ some_str = 'Hello my dear friends!'
+ some_value = 10
+ #передаем данные в шаблон и вызываем его
+ return render_template('simple.html',some_str = some_str,
+ some_value = some_value,some_pars=some_pars) 
+>>>>>>> Stashed changes
