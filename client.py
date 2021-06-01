3696772616d2d9970629a5ad200ad1608d33a8e3
S4B1K <83269819+S4B1K24@users.noreply.github.com>
2021-06-01 21:23:59 +0700

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,11 @@
+import os
 import requests
-r = requests.get('http://localhost:5000/')
-print(r.status_code)
-print(r.text)
-r = requests.get('http://localhost:5000/data_to') 
-print(r.status_code)
-print(r.text) 
+from io import BytesIO
+import base64
+img_data = None
+path = os.path.join('./static','22.jpg')
+with open(path, 'rb') as fh:
+img_data = fh.read()
+b64 = base64.b64encode(img_data)
+jsondata = {'imagebin':b64.decode('utf-8')}
+
