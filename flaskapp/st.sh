dc0f2200c0f7d389141b3f800c347cc733f32042
S4B1K <83269819+S4B1K24@users.noreply.github.com>
2021-06-02 20:59:25 +0700

    Update st.sh

--- flaskapp/st.sh
+++ flaskapp/st.sh
@@ -1,9 +1,11 @@
 gunicorn --bind 127.0.0.1:5000 wsgi:app & APP_PID=$!
-sleep 5
+sleep 25
 echo start client
 python3 client.py
+APP_CODE=$?
 sleep 5
-echo $APP_PID
+echo $APP_PID 
 kill -TERM $APP_PID
-exit 0
+echo app code $APP_CODE
+exit $APP_CODE 
 
